7b90bfd674d7950a436ca483a183a3d172684ed6
David Rodenas <david.rodenas@gmail.com>
2018-05-04 08:51:29 +0200

    SLIDE 24

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -10,18 +10,26 @@ import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest {
 
+    private Game g;
+
+    @Before
+    public void setUp() {
+        g = new Game();
+    }
+
     @Test
     public void gutterGame() {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(0);
+        int n = 20;
+        int pins = 0;
+        for (int i = 0; i< n; i++) {
+            g.roll(pins);
+        }
 
         assertThat(g.score(), is(0));
     }
 
     @Test
     public void testAllOnes() throws Exception {
-        Game g = new Game();
         for (int i = 0; i < 20; i++)
             g.roll(1);
 
