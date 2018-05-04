d6e4fd03046e47a4dfdfe699da5638270a637995
David Rodenas <david.rodenas@gmail.com>
2018-05-04 08:52:29 +0200

    SLIDE 25

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -21,17 +21,20 @@ public class BowlingGameTest {
     public void gutterGame() {
         int n = 20;
         int pins = 0;
-        for (int i = 0; i< n; i++) {
-            g.roll(pins);
-        }
+        rollMany(n, pins);
 
         assertThat(g.score(), is(0));
     }
 
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i< n; i++) {
+            g.roll(pins);
+        }
+    }
+
     @Test
     public void testAllOnes() throws Exception {
-        for (int i = 0; i < 20; i++)
-            g.roll(1);
+        rollMany(20, 1);
 
         assertThat(g.score(), is(20));
     }
